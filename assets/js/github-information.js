57d0d73497fab8b031585f03b33f501aaf3c86b0
lilychuang78 <lilychuang78@gmail.com>
2020-09-21 13:11:27 +0000

    API Throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -64,7 +64,12 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            } else {
+            }
+            else if(errorResponse.status ==403){ //403 means forbidden
+                var resetTime =new Date(errorResponse.getResponseHeader('X-RateLimit-Rest')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } 
+            else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
